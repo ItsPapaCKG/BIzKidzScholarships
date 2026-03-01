2431eecdd3857d1a960d647c91dd3abcfb48785c
ItsPapaCKG <grantsputnam@gmail.com>
2026-02-28 21:27:19 -0500

    ForwardedHeaders - 2.5 hrs

--- BIzKidzScholarships.API/Program.cs
+++ BIzKidzScholarships.API/Program.cs
@@ -55,6 +55,8 @@ namespace BizKidzScholarships.API
                 app.UseSwaggerUI();
             }
 
+            app.UseForwardedHeaders();
+
             app.UseHttpsRedirection();
 
             app.UseCors("frontend");
